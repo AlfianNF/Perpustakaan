dc601e6f3b0527c44d9d096dc65a078b23f81973
AlfianNF <alfiannurfariq@gmail.com>
2025-02-25 14:43:23 +0700

    merge

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,16 +25,12 @@ class AuthController extends Controller
             'password' => Hash::make($validated['password']),
             'is_admin' => false, 
         ]);
-        
-        $token = $user->createToken('auth_token')->plainTextToken;
-
 
         return response()->json([
             'success' => true,
             'message' => 'User berhasil dibuat',
             'data' => [
                 'user' => $user,
-                'token' => $token
             ],
         ], 201);
     }
